197293b9e0d3c4e52ca2d8f3a16c26affbad3f03
[Marek] <marekkabala1@gmail.com>
2023-03-30 06:26:10 +0100

    implemented few changes in css layout,new rout signUp  added component not finisched

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Error } from '@/components/Error';
-import Layout from '@/layouts/layouts';
+import Layout from '@/layouts/Layout';
+import SignUp from '@/components/SignUp';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Layout />,
 		errorElement: <Error />,
+		children: [
+			{
+				path: 'signUp',
+				element: <SignUp />,
+			},
+		],
 	},
 ]);
 
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import { Link, Outlet } from 'react-router-dom';
 import '@@/layout.css';
 import Logo from '@/components/Logo';
+import { AiOutlineLogin, AiOutlineUserAdd } from 'react-icons/ai';
 
 const Layout = () => {
 	return (
@@ -11,12 +12,14 @@ const Layout = () => {
 				</Link>
 				<nav className='header__nav'>
 					<div className='btn btn--small'>
-						<Link className='header__btn' to={'/login'}>
+						<Link className='header__btn--link' to={'/login'}>
+							<AiOutlineLogin />
 							LogIn
 						</Link>
 					</div>
 					<div className='btn btn--small'>
-						<Link className='header__btn' to={'/register'}>
+						<Link className='header__btn--link' to={'/signUp'}>
+							<AiOutlineUserAdd />
 							signUp
 						</Link>
 					</div>
